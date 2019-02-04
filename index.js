c48fdf7f1affe9ec34efa13db770aa6716419193
Gennaro Landolfi <rinodrummer@yahoo.it>
2019-02-04 17:12:13 +0100

    Now the mane of resulting file is the same as the one specified by the user

--- index.js
+++ index.js
@@ -36,8 +36,9 @@ module.exports = bundler => {
     distDir = outDir.replace(rootDir, '').substr(1)
 
     bundler.on('bundled', (bundle) => {
-        const serviceWorkerFilePath = path.resolve(outDir, DEFAULT_FILENAME)
         const customOptions = bundle.entryAsset.package.sw
+        const serviceWorkerFilePath = path.resolve(outDir, customOptions.fileName)
+
         getServiceWorker({ outDir, customOptions, rootDir }).then(codes => {
             if (customOptions.minify) {
                 const compressedCodes = {}
